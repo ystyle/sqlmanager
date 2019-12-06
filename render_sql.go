da427e7a501f5055299f35605cf45ccd9cc32cfa
ystyle <lxy5266@live.com>
2019-12-06 19:48:18 +0800

    add render string

--- render_sql.go
+++ render_sql.go
@@ -32,3 +32,26 @@ func (sm *sqlManager) RenderTPLUnSave(name string, data interface{}) string {
 	}
 	return sql
 }
+
+func (sm *sqlManager) RenderTPLString(sql string, data interface{}) (string, error) {
+	t := template.New("sql")
+	_, err := t.Parse(sql)
+	if err != nil {
+		return "", err
+	}
+	var buff bytes.Buffer
+	err = t.Execute(&buff, data)
+	if err != nil {
+		return "", err
+	}
+	return buff.String(), nil
+}
+
+func (sm *sqlManager) RenderTPLStringUnsave(sql string, data interface{}) string {
+	sql, err := sm.RenderTPL(sql, data)
+	if err != nil {
+		log.Printf("sqlmanager - ERROR: render error: %s", err.Error())
+		return ""
+	}
+	return sql
+}
